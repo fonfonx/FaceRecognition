bc88054aec86a313899e2eedcc1b449003fe34c4
fonfonx <xavier.fontaine@polytechnique.org>
2017-02-25 18:49:02 -0500

    :bug: Use an existing image for landmark exemple

--- landmark.py
+++ landmark.py
@@ -27,18 +27,19 @@ def get_landmarks(img, detect_face=True):
 
 
 def annotate_landmarks(im, landmarks):
-    """ Draw red points corresponding to the landmarks on the image """
+    """ Draw blue points corresponding to the landmarks on the image """
     im = im.copy()
     for idx, point in enumerate(landmarks):
         pos = (point[0], point[1])
+        cv2.circle(im, pos, 3, color=(255, 0, 0))
         cv2.circle(im, pos, 2, color=(255, 0, 0))
         cv2.circle(im, pos, 1, color=(255, 0, 0))
         cv2.circle(im, pos, 0, color=(255, 0, 0))
     return im
 
 
-def get_landmarks_image(image_path, image_path_to_save="./landmarks.jpg", save=False):
-    """ Show and return an image with the facial landmarks and optionally save it"""
+def get_landmarks_image(image_path, save=False, image_path_to_save="./landmarks.jpg"):
+    """ Show and return an image with the facial landmarks and optionally save it """
     im = cv2.imread(image_path)
     im = cv2.resize(im, None, fx=1.0, fy=1.0, interpolation=cv2.INTER_CUBIC)
     im_land = annotate_landmarks(im, get_landmarks(im))
@@ -49,5 +50,5 @@ def get_landmarks_image(image_path, image_path_to_save="./landmarks.jpg", save=F
         cv2.imwrite(image_path_to_save, im_land)
 
 if __name__ == '__main__':
-    im_path = '../lfw2/Angelina_Jolie/Angelina_Jolie_0001.jpg'
-    get_landmarks_image(im_path)
+    im_path = 'federer_image.jpg'
+    get_landmarks_image(im_path, True, "federer_landmarks.jpg")
